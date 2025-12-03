a025956d1b3f7fcbf47a676289675ec6b013ee9c
Gabriel Guo <gabeguo@x1000c2s1b0n1h0.chn.perlmutter.nersc.gov>
2025-12-02 18:21:06 -0800

    fixed some shaping errors. also, jvp isn't supported, so we use finite difference, although still not 100% sure it's correct. also, need to fix the time shifting stuff.

--- examples/dreambooth/utils/loss.py
+++ examples/dreambooth/utils/loss.py
@@ -2,7 +2,7 @@ import torch
 from utils.two_timestep_inference import FluxPipelineTwoTimestep as FluxPipeline
 
 
-def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_embeds, text_ids, latent_image_ids, vae_scale_factor, height, width):
+def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_embeds, text_ids, latent_image_ids, vae_scale_factor, height, width, dt=5e-3):
     # Predict the noise residual
     def v_func(the_x_t0, the_t0, the_t1):
         if the_t1 is not None:
@@ -20,24 +20,28 @@ def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_e
             img_ids=latent_image_ids,
             return_dict=False,
         )[0]
-        v_t0_t1 = FluxPipeline._unpack_latents(
-            v_t0_t1,
-            height=height * vae_scale_factor,
-            width=width * vae_scale_factor,
-            vae_scale_factor=vae_scale_factor,
-        )
+        # v_t0_t1 = FluxPipeline._unpack_latents(
+        #     v_t0_t1,
+        #     height=height * vae_scale_factor,
+        #     width=width * vae_scale_factor,
+        #     vae_scale_factor=vae_scale_factor,
+        # )
         return v_t0_t1
-    v_t0_t1, d_t1_v_t0_t1 = torch.func.jvp(
-        v_func, 
-        primals=(x_t0, t0, t1,),
-        tangents=(torch.zeros_like(x_t0), torch.zeros_like(t0), torch.ones_like(t1),),
-    )
+    v_t0_t1 = v_func(x_t0, t0, t1)
     x_t1 = x_t0 + (t1 - t0) * v_t0_t1
 
-    with transformer.disable_adapters(), torch.no_grad():
+    transformer.module.disable_adapters()
+    with torch.no_grad():
         # recover the original model
         v_t1_t1 = v_func(x_t1, t1, None).detach()
-    transformer.enable_adapters()
+    transformer.module.enable_adapters()
+    
+    # TODO: fix time shifting effect on these calculations
+    # TODO: check about scaling on dt
+    # TODO: can try central difference for more accuracy
+    d_t1_v_t0_t1 = (
+        v_func(x_t0, t0, t1 + dt * 1000) - v_t0_t1
+    ) / dt
 
     diff = v_t0_t1 + (t1 - t0) * d_t1_v_t0_t1 - v_t1_t1.detach()
     # TODO: weighting?

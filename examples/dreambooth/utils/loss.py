11470110072d5d758bf3e3f042c71541526bf6f6
Gabriel Guo <gabeguo@x1000c0s0b0n1h0.chn.perlmutter.nersc.gov>
2025-12-04 10:24:58 -0800

    realized that we need to scale the timesteps to be between 0-1

--- examples/dreambooth/utils/loss.py
+++ examples/dreambooth/utils/loss.py
@@ -7,9 +7,9 @@ def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_e
     # Predict the noise residual
     def v_func(the_x_t0, the_t0, the_t1):
         if the_t1 is not None:
-            timestep = torch.stack([the_t0 / 1000, the_t1 / 1000], dim=-1)
+            timestep = torch.stack([the_t0, the_t1], dim=-1)
         else:
-            timestep = the_t0 / 1000
+            timestep = the_t0
         v_t0_t1 = transformer(
             hidden_states=the_x_t0,
             # YiYi notes: divide it by 1000 for now because we scale it by 1000 in the transformer model (we should not keep it but I want to keep the inputs same for the model for testing)
@@ -22,6 +22,8 @@ def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_e
             return_dict=False,
         )[0]
         return v_t0_t1
+    t0 = t0 / 1000
+    t1 = t1 / 1000
     v_t0_t1 = v_func(x_t0, t0, t1)
     x_t1 = x_t0 + (t1 - t0) * v_t0_t1
 
@@ -35,7 +37,7 @@ def lsd_loss(transformer, x_t0, t0, t1, guidance, pooled_prompt_embeds, prompt_e
     # TODO: double-check about scaling on dt, but I think this is the right way
     # TODO: can try central difference for more accuracy
     d_t1_v_t0_t1 = (
-        v_func(x_t0, t0, t1 + dt * 1000) - v_t0_t1
+        v_func(x_t0, t0, t1 + dt) - v_t0_t1
     ) / dt
     d_t1_v_t0_t1 = d_t1_v_t0_t1.detach() # TODO: can remove if we have more memory
 

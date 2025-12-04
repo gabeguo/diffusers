fb7fc5b21aea5c5b02890921d8e5426faf5f03d4
Gabriel Guo <gabeguo@x1000c0s6b0n0h0.chn.perlmutter.nersc.gov>
2025-12-03 16:28:23 -0800

    some other settings

--- examples/dreambooth/_dummy_finetune_flux.sh
+++ examples/dreambooth/_dummy_finetune_flux.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --partition=m1266
+#SBATCH --account=m1266
+#SBATCH --job-name=flux-finetune
+#SBATCH --constraint=gpu
+#SBATCH --qos=regular
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1                # Single node
+#SBATCH --gpus=a100:4
+#SBATCH --cpus-per-task=8       # CPUs for the job
+#SBATCH --ntasks=4            # Number of tasks (one per GPU)
+
 export MODEL_NAME="black-forest-labs/FLUX.1-dev"
 export INSTANCE_DIR="does_not_exist"
-export OUTPUT_DIR="try-lsd"
+export OUTPUT_DIR="lsd-finetune"
 export HF_HOME="/pscratch/sd/g/gabeguo/cache/huggingface"
 
 accelerate launch train_dreambooth_lora_flux.py \
@@ -12,10 +25,10 @@ accelerate launch train_dreambooth_lora_flux.py \
   --resolution=256 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
-  --learning_rate=1e-3 \
+  --learning_rate=1e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=1000 \
+  --max_train_steps=10000 \
   --seed="0" \
   --cache_dir="/pscratch/sd/g/gabeguo/cache/huggingface" \
   --streaming \
